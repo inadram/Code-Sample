92f20b8898dd4359343b890070c9d8346f03aae8
Amir Mardani <inadram@gmail.com>
2013-02-27 13:36:35 +0000

    TellDontAsk event
    more info at : blog.inadram.com

--- C#/delegates/TellDontAsk/TellDontAsk.cs
+++ C#/delegates/TellDontAsk/TellDontAsk.cs
@@ -1,17 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using NUnit.Framework;
 
 namespace TellDontAsk
 {
     public class TellDontAsk
     {
+        private SendCommunication _sender;
+        public void RegisterCommunication(Message message)
+        {
+            _sender += message.SendMessageWithEmail;
+            _sender += message.SendMessageWithSms;
+        }
+        public void SendMessage(string textMessage, SenderEventHandlerArgs args)
+        {
+            _sender(textMessage, args);
+        }
+
+    }
+    public delegate void SendCommunication(string message, SenderEventHandlerArgs args);
+
+    public class Message
+    {
+        public Message()
+        {
+            SenderEvent = (s, e) => { };
+        }
+        public void SendMessageWithEmail(string message, SenderEventHandlerArgs args)
+        {
+            Console.WriteLine("Email this message {0}", message);
+            //sending message with email
+            args.IsEmailSent = true;
+            SenderEvent(this, args);
+        }
+
+        public void SendMessageWithSms(string message, SenderEventHandlerArgs args)
+        {
+            Console.WriteLine("Sms this mesksage {0}", message);
+            //sending message with Sms
+            args.IsSmsSent = true;
+            SenderEvent(this, args);
+        }
+
+        public event EventHandler<SenderEventHandlerArgs> SenderEvent;
     }
 
-    public interface ICommunication
+    public class SenderEventHandlerArgs : EventArgs
     {
-        void SendMessageWithEmail(string message);
-        void SendMessageWithSms(string message);
+        public bool IsEmailSent { get; set; }
+        public bool IsSmsSent { get; set; }
+    }
+
+
+    [TestFixture]
+    public class TestTellDontAsk
+    {
+        [Test]
+        public void Given_Message_When_SendCommunication_Then_ShouldSendWithSmsAndEmail()
+        {
+            var tellDontAsk = new TellDontAsk();
+            tellDontAsk.RegisterCommunication(new Message());
+            var senderEventHandlerArgs = new SenderEventHandlerArgs { IsEmailSent = false, IsSmsSent = false };
+            tellDontAsk.SendMessage("test message", senderEventHandlerArgs);
+            Assert.IsTrue(senderEventHandlerArgs.IsEmailSent);
+            Assert.IsTrue(senderEventHandlerArgs.IsSmsSent);
+
+        }
     }
 }
