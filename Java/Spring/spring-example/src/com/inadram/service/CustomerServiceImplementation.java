aa55b3b32f7892a799d7b77f52aca59c1acdd3c1
inadram <inadram@gmail.com>
2014-05-07 09:01:37 +0100

    constructor injection in spring

--- Java/Spring/spring-example/src/com/inadram/service/CustomerServiceImplementation.java
+++ Java/Spring/spring-example/src/com/inadram/service/CustomerServiceImplementation.java
@@ -2,15 +2,20 @@ package com.inadram.service;
 
 import com.inadram.customer.Customer;
 import com.inadram.repo.CustomerRepository;
+import com.inadram.repo.hibernateRepositoryImplementation;
 
 import java.util.List;
 
 public class CustomerServiceImplementation implements CustomerService {
 	private CustomerRepository customerRepository;
 
-	public void setCustomerRepository(CustomerRepository customerRepository) {
+	public CustomerServiceImplementation(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
+
+//	public void setCustomerRepository(CustomerRepository customerRepository) {
+//		this.customerRepository = customerRepository;
+//	}
 
 	@Override
 	public List<Customer> findAll() {
