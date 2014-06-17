88c9ed5a1331360e5a8e1162897698f553040289
inadram <inadram@gmail.com>
2014-06-17 09:08:49 +0100

    read property file
    read property file from java configure file in spring

--- Java/Spring/spring-java-autowring-example/src/AppConfig.java
+++ Java/Spring/spring-java-autowring-example/src/AppConfig.java
@@ -2,15 +2,18 @@ import com.inadram.repo.CustomerRepository;
 import com.inadram.repo.hibernateRepositoryImplementation;
 import com.inadram.service.CustomerService;
 import com.inadram.service.CustomerServiceImplementation;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.inadram"})
+@PropertySource("app.someProperty")
 public class AppConfig {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	@Bean(name = "customerService")
 //	@Scope("singleton")
 	@Scope("prototype")
--- Java/Spring/spring-java-autowring-example/src/com/inadram/repo/hibernateRepositoryImplementation.java
+++ Java/Spring/spring-java-autowring-example/src/com/inadram/repo/hibernateRepositoryImplementation.java
@@ -3,6 +3,7 @@ package com.inadram.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.inadram.customer.Customer;
@@ -10,12 +11,16 @@ import org.springframework.stereotype.Service;
 
 public class hibernateRepositoryImplementation implements CustomerRepository {
 
+	@Value("${someValue}")
+	private String someValue;
+
 	@Override
 	public List<Customer> findAll() {
 		List<Customer> customers = new ArrayList<Customer>();
 
 		Customer customer = new Customer();
-		customer.setFirstName("Amir");
+//		customer.setFirstName("Amir");
+		customer.setFirstName(someValue);
 		customer.setLastName("Mardani");
 		customers.add(customer);
 		return customers;
